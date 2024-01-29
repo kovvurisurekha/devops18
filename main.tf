8a77118bc49edc6841bdf6f7a333b7c9bbf7d5fe
kovvurisurekha <140402059+kovvurisurekha@users.noreply.github.com>
2024-01-29 13:03:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_launch_configuration" "web_server_as" {
-    image_id           = "ami-00b8917ae86a424c9"
+    image_id           = "ami-00952f27cf14db9cd"
     instance_type = "t2.micro"
-    key_name = "ansible-kp"
+    key_name = "MY-KEY-PAIR"
 }
    
 
@@ -9,7 +9,7 @@ resource "aws_launch_configuration" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-06bf36ef35554f526", "subnet-04215682caa464cc3"]
+     subnets = ["subnet-0402d3b67bc42d943", "subnet-0b9f30d4a50ed684a"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
